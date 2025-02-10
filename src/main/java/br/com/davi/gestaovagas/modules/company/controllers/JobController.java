5913424f639b79a834929c8b50037fb23d936126
Davi <mdavi6085@gmail.com>
2025-02-10 15:21:08 -0300

    refatorando o job

--- src/main/java/br/com/davi/gestaovagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/davi/gestaovagas/modules/company/controllers/JobController.java
@@ -1,13 +1,18 @@
 package br.com.davi.gestaovagas.modules.company.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.davi.gestaovagas.modules.company.dto.JobDTO;
 import br.com.davi.gestaovagas.modules.company.entities.JobEntity;
 import br.com.davi.gestaovagas.modules.company.useCase.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -18,7 +23,17 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping("/")
-    public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
-        return this.createJobUseCase.execute(jobEntity);
+    @PreAuthorize("hasRole(COMPANY)")
+    public JobEntity create(@Valid @RequestBody JobDTO jobDTO, HttpServletRequest request) {
+        var companyId = request.getAttribute("company_Id");
+
+            var jobEntity = JobEntity.builder()
+                .benefits(jobDTO.getBenefits())
+                .description(jobDTO.getDescription())
+                .level(jobDTO.getLevel())
+                .companyId(UUID.fromString(companyId.toString()))
+                .build();
+
+            return this.createJobUseCase.execute(jobEntity);
     }
 }
---
+++ src/main/java/br/com/davi/gestaovagas/modules/company/dto/JobDTO.java
@@ -0,0 +1,11 @@
+package br.com.davi.gestaovagas.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class JobDTO {
+    
+    private String description;
+    private String benefits;
+    private String level;
+}
